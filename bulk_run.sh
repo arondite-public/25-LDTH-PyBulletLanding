bc3de09693d29a72220f685df39cdc59873ec383
Seb <seb.fenton@arondite.tech>
2025-05-09 23:53:07 +0100

    Swap to SS2
    Signed-off-by: Seb <seb.fenton@arondite.tech>

--- bulk_run.sh
+++ bulk_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for difficulty in "SS0" "SS1" "SS3" "SS5" "SS7"; do
+for difficulty in "SS0" "SS2" "SS3" "SS5" "SS7"; do
     uv run python run_simulation.py --difficulty "$difficulty"
 done
